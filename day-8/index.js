823b2cb2db56d48db99b3478f20926a3609df643
Michał Stolarczyk <cimlahcimlah@gmail.com>
2023-12-08 20:10:07 +0100

    Part 2 - not working yet
    Co-authored-by: lemmoor <lemmoor@users.noreply.github.com>

--- day-8/index.js
+++ day-8/index.js
@@ -9,22 +9,51 @@ const nodes = input[0].split(/\n/).reduce((map, line) => {
     return map;
 }, new Map())
 
-console.log(nodes);
+// console.log(nodes);
 
 let currentNode = 'AAA';
 let steps = 0;
+// for(let i = 0; i < directions.length; i++){
+//     // console.log({i, currentNode, dir: directions[i], next:nodes.get(currentNode)[directions[i]] });
+//     currentNode = nodes.get(currentNode)[directions[i]]
+//     // console.log(currentNode);
+//     if(currentNode == 'ZZZ'){
+//         steps += i + 1;
+//         break;
+//     }
+//     if(i == directions.length -1){
+//         steps += i + 1;
+//         i = -1;
+//     }
+// }
+
+console.log(currentNode, steps);
+
+let currentNodes = [...nodes.keys()].filter(node => node.at(-1) === 'A')
+let steps2 = 0;
+let debug = 0;
+console.log(currentNodes);
 for(let i = 0; i < directions.length; i++){
     // console.log({i, currentNode, dir: directions[i], next:nodes.get(currentNode)[directions[i]] });
-    currentNode = nodes.get(currentNode)[directions[i]]
-    // console.log(currentNode);
-    if(currentNode == 'ZZZ'){
-        steps += i + 1;
+    currentNodes = currentNodes.map(node => nodes.get(node)[directions[i]])
+    // console.log(currentNodes);
+    if(currentNodes.every(node => node.at(-1) === 'Z')){
+        steps2 += i + 1;
         break;
     }
     if(i == directions.length -1){
-        steps += i + 1;
+        debug++;
+        // console.log('step');
+        steps2 += i + 1;
         i = -1;
     }
+    // if(debug > 99999){
+    //     console.log('debug');
+    //     break;
+    // }
 }
 
-console.log(currentNode, steps);
+console.log(steps2);
+
+//28300283 - too low
+//283000283
